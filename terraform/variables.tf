4c6fe2470fa5277cf6cf473eeab5d04edc434572
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2024-10-05 15:19:36 -0700

    error due to var duplication

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,4 @@
 # variables.tf
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-}
-
 variable "resource_group_name" {
   description = "Name of the Resource Group"
   default     = "terraform-aks-rg"
