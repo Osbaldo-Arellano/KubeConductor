fa7a79246242d597c9e9b189b36356fe2076cbcd
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2024-10-05 15:11:17 -0700

    terraform was failing on step 4

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -1,7 +1,13 @@
 # aks-cluster.tf
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
+
 
 resource "azurerm_resource_group" "default" {
   name     = "terraform-aks-rg"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,20 @@ variable "resource_group_name" {
   description = "Name of the Resource Group"
   default     = "terraform-aks-rg"
 }
+
+variable "client_id" {
+  description = "The Client ID of the Service Principal"
+}
+
+variable "client_secret" {
+  description = "The Client Secret of the Service Principal"
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID of the Azure Active Directory"
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID where the resources will be created"
+}
+
